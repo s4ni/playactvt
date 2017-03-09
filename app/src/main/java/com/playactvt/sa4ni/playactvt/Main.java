2439166c88b13364d0a64ba4590acef43b2c6493
sa4ni <khalil.s4ni@gmail.com>
2017-03-09 17:01:01 +0530

    code decrease

--- app/src/main/java/com/playactvt/sa4ni/playactvt/Main.java
+++ app/src/main/java/com/playactvt/sa4ni/playactvt/Main.java
@@ -1,5 +1,6 @@
 package com.playactvt.sa4ni.playactvt;
 
+import android.content.ComponentName;
 import android.content.Intent;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
@@ -31,8 +32,7 @@ public class Main extends AppCompatActivity {
     private List<ApplicationInfo> all_app;
     int count=0;
     private GridviewAdapter GVA_avtvt;
-    private ArrayList<String> al_nm;
-    private ArrayList<String> al_lbl;
+    private ArrayList<String> al_nm,al_lbl;
     private ArrayList<Drawable> lst_icn;
 
     @Override
@@ -41,13 +41,10 @@ public class Main extends AppCompatActivity {
         setContentView(R.layout.main);
 
         grd_main=(GridView)findViewById(R.id.GRD_mian);
-        grd_main.setAdapter(GVA_avtvt);
-
         tv_Hw = (TextView)findViewById(R.id.tvHw);
         btn_submit = (Button)findViewById(R.id.btn_Submit);
         et_app = (EditText)findViewById(R.id.etApp);
 
-        Intent intent = new Intent(Intent.ACTION_MAIN, null);
         lst_icn = new ArrayList<Drawable>();
         al_nm = new ArrayList<String>();
         al_lbl = new ArrayList<String>();
@@ -59,14 +56,11 @@ public class Main extends AppCompatActivity {
             Drawable icn = pmp.getApplicationIcon(appinfo);
             al_nm.add(str_nm);
             lst_icn.add(icn);
+            int lngth = all_app.get(count).toString().length();
+            String str_lbl = all_app.get(count).toString().substring(24, lngth - 1);
+            al_lbl.add(str_lbl);
             count = count + 1;
 
-        }
-        for (int i=0; i < count; i++) {
-
-            int lngth = all_app.get(i).toString().length();
-            String str_lbl = all_app.get(i).toString().substring(24, lngth - 1);
-            al_lbl.add(str_lbl);
         }
         GVA_avtvt=new GridviewAdapter(this,al_nm,al_lbl, lst_icn);
         grd_main.setAdapter(GVA_avtvt);
@@ -76,7 +70,6 @@ public class Main extends AppCompatActivity {
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
                 String sel_app = GVA_avtvt.getItem(position);
-                Log.d(sel_app," Clicked");
                 et_app.setText(sel_app);
             }
         });
@@ -90,7 +83,6 @@ public class Main extends AppCompatActivity {
                     if (et_app.getText().toString().equals(al_nm.get(i).toString())) {
 
                         str_pkg = al_lbl.get(i);
-                        Log.d("Cached Package", str_pkg);
                         break;
 
                     }else{
@@ -100,8 +92,8 @@ public class Main extends AppCompatActivity {
                 if(str_pkg != ""){
 
                     Log.d("Cached", str_pkg);
-                    Intent intent = pmp.getLaunchIntentForPackage(str_pkg);//com.android.deskclock
-                    startActivity(intent);
+                    Intent i = pmp.getLaunchIntentForPackage(str_pkg);
+                    startActivity(i);
                     Toast.makeText(getApplicationContext(), et_app.getText().toString()+" Loaded", Toast.LENGTH_SHORT).show();
 
                 }else if(str_pkg == ""){
@@ -111,4 +103,3 @@ public class Main extends AppCompatActivity {
         });
     }
 }
-//Intent { act=android.intent.action.MAIN cat=[android.intent.category.LAUNCHER] flg=0x10000000 pkg=com.android.deskclock cmp=com.android.deskclock/.DeskClock }
